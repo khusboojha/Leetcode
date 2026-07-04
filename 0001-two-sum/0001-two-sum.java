737df2f7fd52cd7b92040ed0381401953ece8793
KHUSBOO JHA <73184499+khusboojha@users.noreply.github.com>
2026-07-04 21:05:56 +0530

    Time: 2 ms (99.36%), Space: 47.1 MB (39.54%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,16 +1,17 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         
-        for (int i=0; i < nums.length; i++)
+        HashMap<Integer, Integer> hashmap = new HashMap <>();
+        for (int i=0; i<nums.length; i++)
         {
-            for (int j=i+1; j< nums.length; j++)
+            int complement = target - nums[i];
+            if(hashmap.containsKey(complement))
             {
-                if (nums[i] + nums[j] == target)
-                {
-                    return new int[] {i,j};
-                }
+                return new int[] {hashmap.get(complement), i};
             }
+            hashmap.put(nums[i], i);
         }
-        return new int[] {};
+        return null;
+
     }
 }
